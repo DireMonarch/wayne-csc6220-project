d8c9127fea6662d56bbdf1d1cd264600c6bee3ad
Jim Haslett <haslettj@wayne.edu>
2024-11-17 12:30:23 -0500

    Updated execute script to run full set of tests

--- execute.sh
+++ execute.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-
-mpirun -n 8 ./jim-haslett-csc6220-project-standard $1
-mpirun -n 8 ./jim-haslett-csc6220-project-variant $1
-./jim-haslett-csc6220-project-serial-quicksort $1
+for ARRAY_SIZE_POWER in 16 20 24
+do 
+    ./jim-haslett-csc6220-project-serial-quicksort $ARRAY_SIZE_POWER
+    mpirun -n 8 ./jim-haslett-csc6220-project-standard $ARRAY_SIZE_POWER
+    mpirun -n 8 ./jim-haslett-csc6220-project-variant $ARRAY_SIZE_POWER
+done # ARRAY_SIZE_POWER
